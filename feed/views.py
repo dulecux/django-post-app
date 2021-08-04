32dd45fdffc926d2f605fe3ac97b8efe33d77e33
dulecux <dusan.culibrk@live.com>
2021-08-04 02:18:09 +0200

    Post Create View and Forcing Auth

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -17,3 +19,21 @@ class PostDetailView(DetailView):
     template_name = "feed/detail.html"
     model = Post
     context_object_name = "post"
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "feed/create.html"
+    fields = ['text']
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
+
+    
